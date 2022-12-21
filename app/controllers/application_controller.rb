55caff6db7d0645f578a5e5c68728734ebc3bf58
jbcsantos028 <jbcsantos028@gmail.com>
2022-12-21 12:14:12 +0800

    moved logic for setting a tenant to a set_tenant concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  set_current_tenant_through_filter #acts_as_tenant
-  before_action :set_organization
-
-  def set_organization
-    if current_user
-      current_organization = current_user.organization
-      set_current_tenant(current_organization)
-    else
-      set_current_tenant(nil)
-    end
-  end
+  include SetOrganization
+  
 end
