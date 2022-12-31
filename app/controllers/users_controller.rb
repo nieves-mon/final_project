c5967fab17a245f58dbd5d698ec3d558e2be432e
jbcsantos028 <jbcsantos028@gmail.com>
2022-12-31 08:49:37 +0800

    Restricted user's show, edit and destroy actions only to Admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   include RequireOrganization #if no organization is set, access will not be allowed to whole UsersController
 
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_admin, only: [:show, :edit, :update, :destroy]
   
   def index
     @users = User.all
@@ -39,13 +40,21 @@ class UsersController < ApplicationController
 
   private
     
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    
-    def user_params
-      params.require(:user).permit(:email, *User::ROLES)
+  
+  def user_params
+    params.require(:user).permit(:email, *User::ROLES)
+  end
+
+  def require_admin
+    if current_user.admin?
+      # allow to proceed
+    else
+      redirect_to users_path, alert: "You are not authorized to perform this action."
     end
+  end
 
 end
