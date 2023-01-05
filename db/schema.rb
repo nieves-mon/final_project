fc8268bdd85fdbf51e46fa2214aab7341db034ea
nieves-mon <cunanan.nm@gmail.com>
2023-01-05 20:07:49 +0800

    Add zoom_link column to meetings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_04_025620) do
+ActiveRecord::Schema.define(version: 2023_01_05_120536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2023_01_04_025620) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "scheduled_date"
+    t.string "zoom_link"
     t.index ["organization_id"], name: "index_meetings_on_organization_id"
   end
 
