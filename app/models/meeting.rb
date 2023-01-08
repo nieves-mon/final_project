92afc13f40dfda47ac4ee9238035d0468afa95ed
nieves-mon <cunanan.nm@gmail.com>
2023-01-09 06:24:45 +0800

    Refactor zoom_meeting creation to save meeting id

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -7,12 +7,12 @@ class Meeting < ApplicationRecord
     zoom_client = Zoom.new
     response = zoom_client.meeting_create(topic: topic, user_id: Rails.application.credentials.zoom[:user_id],
                                           settings: {start_time: start_time, join_before_host: true})
-    response['join_url']
+    [response['join_url'], response['id']]
   end
 
-  def save_zoom_link(meeting_topic, start_time)
+  def save_zoom_meeting(meeting_topic, start_time)
     return if zoom_link.present?
-    _zoom_link = create_zoom_meeting(meeting_topic, start_time)
-    update(zoom_link: _zoom_link)
+    _zoom_link, _zoom_id = create_zoom_meeting(meeting_topic, start_time)
+    update(zoom_link: _zoom_link, zoom_id: _zoom_id)
   end
 end
