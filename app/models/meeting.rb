739a69475adae4cb2d2ed96225f26919da15b558
jbcsantos028 <jbcsantos028@gmail.com>
2023-01-09 17:25:16 +0800

    changed belongs_to to acts_as_tenant in Meeting.rb

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,5 +1,6 @@
 class Meeting < ApplicationRecord
-  belongs_to :organization
+  # belongs_to :organization
+  acts_as_tenant(:organization)
   has_many :user_meetings, dependent: :destroy
   has_many :users, through: :user_meetings, dependent: :destroy
 end
