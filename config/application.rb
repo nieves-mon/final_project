c349606f0cab75d7483c8a65d7603ce62296756f
nieves-mon <cunanan.nm@gmail.com>
2023-01-11 18:18:00 +0800

    Set precompile to false

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module FinalProject
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.initialize_on_precompile = false
   end
 end
