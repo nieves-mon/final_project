97282a794872c0797a6a01ea85d3bb928138bce3
nieves-mon <cunanan.nm@gmail.com>
2023-01-12 16:36:03 +0800

    Add validations for title, duedate, and user_id

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,9 @@ class Task < ApplicationRecord
   belongs_to :project
   belongs_to :user
 
+  validates :title, presence: true, length: { minimum: 2, maximum: 25}
+  validates :duedate, :user_id, presence: true
+
   scope :pending, -> { where("completed = ?", false).order(:duedate) }
   scope :overdue, -> { pending.where("duedate < ?", Date.current) }
   scope :today, -> { pending.where("duedate = ?", Date.current) }
