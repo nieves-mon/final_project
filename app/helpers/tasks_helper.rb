aa7534e3baa302e07512de71a37e3da60d6976a5
nieves-mon <cunanan.nm@gmail.com>
2023-01-12 19:01:58 +0800

    Refactor user reference in task to become nullable

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,7 @@
 module TasksHelper
+    def task_assignee_options(project)
+        options = project.users.reduce({}) { |hash, user| hash[user.email] = user.id; hash; }
+        options["Don't Assign Yet"] = 0
+        options
+    end
 end
