f9b3c5758da72e1c2f381703579b105a028c0812
nieves-mon <cunanan.nm@gmail.com>
2023-01-13 21:06:42 +0800

    Resolve conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -29,20 +29,11 @@ Rails.application.routes.draw do
           #get "delete_people", to: "project_participants#delete_people"
           #delete "delete_people", to: "project_participants#delete_people"
 
-<<<<<<< HEAD
-        # get "project_participants" => "project_participants#delete_people", as: :delete_people
-=======
         get "project_participants" => "project_participants#delete_people", as: :delete
->>>>>>> 6d9f985d9727aff8873a3d094b025ad867d25e38
         #post "project_participants/delete" => "participants#destroy"
         end
     end
   end
 
   #get "delete_people", to: "project_participants#delete_people"
-<<<<<<< HEAD
   #delete "delete_people", to: "project_participants#delete_people"
-end
-=======
-  #delete "delete_people", to: "project_participants#delete_people"
->>>>>>> 6d9f985d9727aff8873a3d094b025ad867d25e38
