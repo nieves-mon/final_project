427f1d639c2e7f8a6653a6b08c04b28d536ffd25
nieves-mon <cunanan.nm@gmail.com>
2023-02-13 02:17:30 +0800

    Set card layout in project show

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,4 +3,14 @@ module ProjectsHelper
         user = User.find(user_id)
         user.email
     end
+
+    def project_status(project)
+        if project.complete
+            "Complete"
+        elsif project.end_date < Date.current
+            "Overdue"
+        else
+            "Pending"
+        end
+    end
 end
