5b23b0ea20fc0b47abb2a011975e142220893588
nieves-mon <cunanan.nm@gmail.com>
2023-02-13 01:15:45 +0800

    Revise add participant feature to select available members

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -1,2 +1,6 @@
 module ParticipantsHelper
+    def meeting_participant_options(meeting)
+        options = User.available_for_meeting(meeting).reduce({}) { |hash, user| hash[user.email] = user.email; hash; }
+        options.sort.to_h
+    end
 end
